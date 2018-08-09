4b2f986667ccf9728322fca326a19382d95dc0fe
SpaceHead1C <damh17@gmail.com>
2018-08-09 22:05:55 +0300

    Add in src/app dir 'components' Add components in project with commands: ng g component login-page ng g component signup-page
    ng g component dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-// Firebase * Angularfire2
+// Firebase & Angularfire2
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 // Firebase config
 import { environment } from '../environments/environment';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { SignupPageComponent } from './components/signup-page/signup-page.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
