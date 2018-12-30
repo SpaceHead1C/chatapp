2646b10306177226cf13645b98b714700d1835a1
SpaceHead1C <damh17@gmail.com>
2018-12-30 19:17:27 +0300

    add sign-up form controls & call signUp method (AuthService)

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+
+import { AuthService } from 'src/app/services/auth.service';
+
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 
 @Component({
   selector: 'app-signup-page',
@@ -7,14 +13,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup-page.component.css']
 })
 export class SignupPageComponent implements OnInit {
+  usercreds = {
+    email: '',
+    password: '',
+    displayName: ''
+  };
 
-  constructor(private router: Router) { }
+  emailFormControl: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(EMAIL_REGEX)
+  ]);
+
+  passwordFormControl: FormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
   }
 
   goBack() {
     this.router.navigate(['/login']);
+  }
+
+  createAccount() {
+    this.auth.signUp(this.usercreds);
   }
 
 }
